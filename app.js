27d793648a22809844156f88e49ba75f94ebc630
carlos23fb <jcflobe@gmail.com>
2021-08-29 22:30:33 -0600

    Package.json and npm

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const {dinero, frutas} = require('./fruits');
+const cowsay = require('cowsay');
+// frutas.forEach(fruta  => {
+//     console.count(fruta);
+// })
 
-frutas.forEach(fruta  => {
-    console.count(fruta);
-})
+let dialog = {
+    text: "I'm a moooodule",
+    e: "oO",
+    T: "U"
+}
 
-console.log(dinero);
+
+console.log(cowsay.say(dialog));
