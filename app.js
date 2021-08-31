68bbebdb1f397a22ac815be6512284b328326b24
carlos23fb <jcflobe@gmail.com>
2021-08-31 13:13:38 -0600

    setup public directory for static files and set '/about' and '/' routes

--- app.js
+++ app.js
@@ -11,6 +11,12 @@ app.get('/about', (req, res) => {
     res.send('Esta es la pagina "about"')
 })
 
+app.use(express.static(__dirname + "/public"))
+
 app.listen(port, () => {
     console.log(`Escuchando desde el puerto ${port}`);
+})
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(__dirname + "/public/404.html")
 })
