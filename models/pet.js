f3b603b1e3b2e50fb01fece041f5877cdeb66f27
carlos23fb <jcflobe@gmail.com>
2021-09-09 18:53:58 -0600

    using object destructuring for import Scheme

--- models/pet.js
+++ models/pet.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const { Schema } = mongoose;
 
 const petSchema = new Schema({
-    name : String,
-    descripcion : String
+    name: String,
+    descripcion: String
 })
