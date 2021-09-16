c1ff2340b87b08dacd6253f2f189f7f62830843a
Carlos Becerra <carlos23fb@github.com>
2021-09-16 11:34:05 -0600

    print res infor from delete request

--- public/js/script_delete.js
+++ public/js/script_delete.js
@@ -6,6 +6,11 @@ document.addEventListener('DOMContentLoaded', () => {
         const res = await axios({
             method: 'delete',
             url: `/pets/${id}`,
-        })
+        }).then((res) => {
+            console.log(res.data);
+            console.log(res.status)
+        }).catch((err) => {
+            console.log('error', err)
+        });
 })
 })
