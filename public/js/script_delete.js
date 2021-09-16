a44d0b89c447ca2a8f5390b81f117ba860faeb5c
Carlos Becerra <carlos23fb@github.com>
2021-09-16 14:35:13 -0600

    axios Delete function

--- public/js/script_delete.js
+++ public/js/script_delete.js
@@ -1,16 +1,15 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const button = document.querySelector('#btn-delete')
 
-    button.addEventListener('click', async () => {
-        const id = button.dataset.id
-        const res = await axios({
-            method: 'delete',
-            url: `/pets/${id}`,
-        }).then((res) => {
-            console.log(res.data);
-            console.log(res.status)
-        }).catch((err) => {
-            console.log('error', err)
-        });
-})
-})
+const button = document.querySelector('#btn-delete')
+const id = button.dataset.id
+button.addEventListener('click', async () => {
+    console.log(axiosDelete())
+});
+
+async function axiosDelete () {
+    const res = await axios({
+        method: 'delete',
+        url: `/pets/${id}`,
+    })
+    console.log(res.data)
+    return res.data
+}
